7326f860e40aedb22d31878bd4b4086a3a78f965
Willian Klein de Gois <willian.gois@viasoft.com.br>
2021-01-20 20:02:47 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { PhotosModule } from './photos/photos.module';
+import { PhotoService } from './photos/photo/photo.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    PhotosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
